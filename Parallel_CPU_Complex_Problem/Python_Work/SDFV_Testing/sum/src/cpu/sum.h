95bc199443b36c864e21b0aef908ff1804a5a095
Jeremy <jeremyjwsc@gmail.com>
2021-05-17 21:01:59 +0200

    RP Work

---
+++ Parallel_CPU_Complex_Problem/Python_Work/SDFV_Testing/sum/src/cpu/sum.h
@@ -0,0 +1,3 @@
+extern "C" int __dace_init_sum(float * __restrict__ A, float * __restrict__ out, int N);
+extern "C" int __dace_exit_sum(float * __restrict__ A, float * __restrict__ out, int N);
+extern "C" void __program_sum(float * __restrict__ A, float * __restrict__ out, int N);
